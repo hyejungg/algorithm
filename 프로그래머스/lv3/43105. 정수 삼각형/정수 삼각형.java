cc7cbc1a8aea8559a7bba9b1812c6c75df284da9
hyejung kim <68772751+hyejungg@users.noreply.github.com>
2022-09-09 20:02:46 +0900

    [level 3] Title: 정수 삼각형, Time: 7.97 ms, Memory: 61.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        int[][] temp = new int[triangle.length][triangle.length];
+        temp[0][0] = triangle[0][0];
+
+        for (int i = 1; i < triangle.length; i++) {
+            // 맨 왼쪽
+            temp[i][0] = temp[i - 1][0] + triangle[i][0];
+            // 맨 오른쪽
+            temp[i][i] = temp[i - 1][i - 1] + triangle[i][i];
+            // 중간 (triangle + 좌우 중 큰 값)
+            for (int j = 1; j <= i; j++) {
+                temp[i][j] = triangle[i][j] + Math.max(temp[i - 1][j - 1], temp[i - 1][j]);
+            }
+        }
+        
+        // 가장 마지막 줄 합 중 가장 큰 애가 answer
+        for (int i = 0; i < triangle.length; i++) {
+            answer = Math.max(temp[triangle.length - 1][i], answer);
+        }
+
+        return answer;
+    }
+}
