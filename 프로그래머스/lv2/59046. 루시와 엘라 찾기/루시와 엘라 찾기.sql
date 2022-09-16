fb4755b614ea5d631bc2482b77ffb79ad2305566
hyejung kim <68772751+hyejungg@users.noreply.github.com>
2022-09-17 00:20:57 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -11,3 +11,4 @@
 select animal_id, name, sex_upon_intake
 from animal_ins
 where 'Lucy, Ella, Pickle, Rogan, Sabrina, Mitty' like concat('%', name, '%');
+# concat('%', 칼럼, '%') : 해당 칼럼에 특정 문장려이 있는지 검사
