0055a954f908c251e8f5e67b18285977e0377d19
hyejung kim <68772751+hyejungg@users.noreply.github.com>
2022-09-17 00:20:11 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,9 +1,13 @@
 -- 코드를 입력하세요
+# select animal_id, name, sex_upon_intake
+# from animal_ins
+# where name = 'Lucy' 
+# or name = 'Ella'
+# or name = 'Pickle'
+# or name = 'Rogan'
+# or name = 'Sabrina'
+# or name = 'Mitty';
+
 select animal_id, name, sex_upon_intake
 from animal_ins
-where name = 'Lucy' 
-or name = 'Ella'
-or name = 'Pickle'
-or name = 'Rogan'
-or name = 'Sabrina'
-or name = 'Mitty';
+where 'Lucy, Ella, Pickle, Rogan, Sabrina, Mitty' like concat('%', name, '%');
