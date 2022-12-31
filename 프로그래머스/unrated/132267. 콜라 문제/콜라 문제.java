30730ca10c78fbdbbed52ebbf2d61178d6821beb
hyejung kim <68772751+hyejungg@users.noreply.github.com>
2022-12-31 21:40:03 +0900

    [unrated] Title: 콜라 문제, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b, int n) {
+        if (n < a) {
+            return 0;
+        }
+        int answer = (n / a) * b;
+        return answer + solution(a, b, answer + (n % a));
+    }
+}
